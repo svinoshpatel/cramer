58e31f61390421a6316ac434cb327e52eaba7df0
svinoshpatel <132487936+svinoshpatel@users.noreply.github.com>
2024-10-21 12:05:12 +0300

    Update cramer.pas

--- gui/cramer.pas
+++ gui/cramer.pas
@@ -112,7 +112,7 @@ var
   sublist : Matrix;
 begin
   d := det(system, n);
-  if d < 1e-7 then
+  if abs(d) < 1e-7 then
   begin
     ShowMessage('Determinant equals zero. No result can be found.');
     exit;
